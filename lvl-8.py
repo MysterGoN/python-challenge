1b242ac87ca85f1a560b1e4b0917557ab38df487
Nikolay Granenkov <nickgran@yandex.ru>
2021-03-18 15:42:52 +0300

    add: lvl-8

---
+++ lvl-8.py
@@ -0,0 +1,9 @@
+# Start page: http://www.pythonchallenge.com/pc/def/integrity.html
+
+from bz2 import decompress
+
+us = b"BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084"
+pw = b"BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08"
+
+print("User:", decompress(us))
+print("Password:", decompress(pw))
